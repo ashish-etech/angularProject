dc67d24bfd9e31cde4238f5231db2a3ffb51fe5f
anchalexcellence <anchal@excellencetechnologies.in>
2017-02-28 12:07:01 +0530

    Angular registartion form

---
+++ registrationapp.js
@@ -0,0 +1,25 @@
+var app = angular.module("angularForm", []);
+app.controller("registerCtrl", function($scope, $http, getdatafactory, $timeout) {
+    $scope.alertsuccess = false;
+    $scope.alerterror = false;
+    $scope.errmsg = '';
+    $scope.change = function() {
+        $scope.alerterror = false;
+    }
+    $scope.submit = function(data) {
+        getdatafactory.sendData(data)
+            .success(function(response) {
+                $scope.alertsuccess = true;
+
+                $scope.form.$setPristine();
+                $timeout(function() {
+                    $scope.alertsuccess = false;
+                    $scope.user = {};
+                }, 3000)
+            })
+            .error(function(error) {
+                $scope.alerterror = true;
+                $scope.errmsg = error;
+            })
+    }
+})
