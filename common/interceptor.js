fbb9a2fe4cfc276b7eeaed14ec6c3fd3d9b056a7
anchalexcellence <anchal@excellencetechnologies.in>
2017-03-03 18:30:49 +0530

    Implementation of interceptors

--- common/interceptor.js
+++ common/interceptor.js
@@ -2,17 +2,11 @@ var app = angular.module("angularForm");
 app.factory('Interceptor', function($localStorage) {
     return {
         request: function(config) {
-            if ($localStorage.token) {
-                if (config.url == 'angularUtils.directives.dirPagination.template') {
-                    config.url = config.url
-                } else {
-                    config.url = config.url + '?accessToken=' + $localStorage.token;
-                }
+            if (config.url != 'angularUtils.directives.dirPagination.template') {
+                config.url = config.url + '?accessToken=' + $localStorage.token;
             }
             return config;
         },
-        response: function(res) {
-            return res;
-        }
+
     }
 });
