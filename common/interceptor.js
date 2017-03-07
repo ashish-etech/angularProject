c38cc0845adc4bedba333bb7d15b0bff0e90ab51
manish <manish@excellencetechnologies.in>
2017-03-07 12:30:14 +0530

    searchFilter

--- common/interceptor.js
+++ common/interceptor.js
@@ -2,12 +2,10 @@ var app = angular.module("angularForm");
 app.factory('Interceptor', function($localStorage) {
     return {
         request: function(config) {
-            if (config.url == 'angularUtils.directives.dirPagination.template') {
-               return config
-
+            if (config.url != 'angularUtils.directives.dirPagination.template') {
+               config.url = config.url + '?accessToken=' + $localStorage.token;
             }
-            console.log(config);
-             config.url = config.url + '?accessToken=' + $localStorage.token;
+      
             return config;
         }
     }
